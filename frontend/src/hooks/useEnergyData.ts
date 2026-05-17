61f8df77728b08155474f6fe0e3839159fbf6cc4
Akshaya Balakrishnan <akshaya.b1811@gmail.com>
2026-05-17 22:46:09 +0200

    fix eslint exhaustive-deps warning

--- frontend/src/hooks/useEnergyData.ts
+++ frontend/src/hooks/useEnergyData.ts
@@ -29,6 +29,7 @@ export function useEnergyData(sampleRows: number, seed: number) {
 
   useEffect(() => {
     loadData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return {
